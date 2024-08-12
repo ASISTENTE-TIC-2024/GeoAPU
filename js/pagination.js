69c4c62d872b324dbe1d818650cc0a3376b7a376
Asistente TIC <asistentetic@geopolimerossas.com>
2024-08-12 16:50:07 -0500

    Se termino el modulo para seleccionar proyecto y se hace el nav para el aparado de informacion del proyecto

--- js/pagination.js
+++ js/pagination.js
@@ -1,62 +1,62 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const pages = document.querySelectorAll(".page");
-    const pageNumbers = document.querySelectorAll(".page-number");
-    const prevButton = document.getElementById("prevPage");
-    const nextButton = document.getElementById("nextPage");
-    let currentPage = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    const pages = document.querySelectorAll('.page')
+    const pageNumbers = document.querySelectorAll('.page-number')
+    const prevButton = document.getElementById('prevPage')
+    const nextButton = document.getElementById('nextPage')
+    let currentPage = 0
 
     function showPage(pageNumber) {
         pages.forEach((page, index) => {
             if (index === pageNumber) {
-                page.style.display = "block";
+                page.style.display = 'block'
             } else {
-                page.style.display = "none";
+                page.style.display = 'none'
             }
-        });
+        })
     }
 
     function updateButtons() {
-        prevButton.disabled = currentPage === 0;
-        nextButton.disabled = currentPage === pages.length - 1;
+        prevButton.disabled = currentPage === 0
+        nextButton.disabled = currentPage === pages.length - 1
     }
 
     function setActive() {
         pageNumbers.forEach((page, index) => {
-            if(currentPage === index) {
-                page.classList.add("active");
+            if (currentPage === index) {
+                page.classList.add('active')
             } else {
-                page.classList.remove("active");
+                page.classList.remove('active')
             }
-        });
+        })
     }
 
     pageNumbers.forEach((page, index) => {
-        page.addEventListener("click", function () {
-            showPage(index);
-            currentPage = index;
-            updateButtons();
-            setActive();
-        });
-    });
+        page.addEventListener('click', function () {
+            showPage(index)
+            currentPage = index
+            updateButtons()
+            setActive()
+        })
+    })
 
-    prevButton.addEventListener("click", function () {
+    prevButton.addEventListener('click', function () {
         if (currentPage > 0) {
-            currentPage--;
-            showPage(currentPage);
-            updateButtons();
-            setActive();
+            currentPage--
+            showPage(currentPage)
+            updateButtons()
+            setActive()
         }
-    });
+    })
 
-    nextButton.addEventListener("click", function () {
+    nextButton.addEventListener('click', function () {
         if (currentPage < pages.length - 1) {
-            currentPage++;
-            showPage(currentPage);
-            updateButtons();
-            setActive();
+            currentPage++
+            showPage(currentPage)
+            updateButtons()
+            setActive()
         }
-    });
+    })
 
-    showPage(currentPage);
-    updateButtons();
-});
+    showPage(currentPage)
+    updateButtons()
+})
