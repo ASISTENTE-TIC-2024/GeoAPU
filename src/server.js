2436c317da1a8e4a60be35d86e2b84983e5a9599
Asistente TIC <asistentetic@geopolim.dom>
2025-01-29 07:16:51 -0600

    Se vuelve a corregir la ruta

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'src/views')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
